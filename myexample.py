0f46bf31ab3d828c2cab17320581fd31df4f4266
unknown <cristianpedromorales@gmail.com>
2018-11-26 23:15:40 -0500

    first commit

---
+++ myexample.py
@@ -0,0 +1,9 @@
+print('hello world')
+# using indexes in strings
+new_string = 'abcdefghijklmnop'
+# returns everything after b in new_string
+print(new_string[1:])
+# returns strings efg 
+print(new_string[4:7])
+# returns string dfhj by using a range on where to start and stop and a step
+print(new_string[1:10:2])
